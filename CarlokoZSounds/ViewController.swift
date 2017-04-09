d1fbdc5a5bbd8cab82dd79e478966acf0de4531b
Carlos Rado <carlokoz@node-1w7jr9qrxajwyq6rhi81eb0lz.ipv6.telus.net>
2017-04-08 18:42:23 -0700

    Initial Commit

---
+++ CarlokoZSounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarlokoZSounds
+//
+//  Created by Carlos Rado on 2017-04-08.
+//  Copyright © 2017 Carlos Rado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
